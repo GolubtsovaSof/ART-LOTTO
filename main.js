e0a9de36557d56266310466b7d0ede600960850c
GolubtsovaSof <92886622+GolubtsovaSof@users.noreply.github.com>
2022-07-17 16:24:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,265 @@
+const cardArray = [
+    {
+        name:'Всадница',
+        author:'Брюллов',
+        img:'./images/Брюллов_Всадница.jpg'
+    },
+    {
+        name:'Всадник. Георгий победоносец. Эскиз',
+        author:'Кандинский',
+        img:'./images/Кандинский_Всадник. Георгий победоносец. Эскиз.jpg'
+    },
+    {
+        name:'В лодке',
+        author:'Коровин',
+        img:'./images/Коровин_В лодке.jpg'
+    },
+    {
+        name:'Неизвестная',
+        author:'Крамской',
+        img:'./images/Крамской_Неизвестная.jpg'
+    },
+    {
+        name:'Березовая роща',
+        author:'Куинджи',
+        img:'./images/Куинджи_Берёзовая роща.jpg'
+    },
+    {
+        name:'Масленица',
+        author:'Кустодиев',
+        img:'./images/Кустодиев_Масленица.jpg'
+    },
+    {
+        name:'Золотая осень_Левитан',
+        author:'Левитан',
+        img:'./images/Левитан_Золотая осень.jpg'
+    },
+    {
+        name:'Портрет неизвестной с розой',
+        author:'Левицкий',
+        img:'./images/Левицкий_Портрет неизвестной с розой.jpg'
+    },
+    {
+        name:'Василий Блаженный',
+        author:'Лентулов',
+        img:'./images/Лентулов_Василий Блаженный.jpg'
+    },
+    {
+        name:'Чёрный квадрат',
+        author:'Малевич',
+        img:'./images/Малевич_Чёрный квадрат.jpg'
+    },
+    {
+        name:'Золотая осень_Остроухов',
+        author:'Остроухов',
+        img:'./images/Остроухов_Золотая осень.jpg'
+    },
+    {
+        name:'Рыбак среди скал',
+        author:'Пиросманашвили',
+        img:'./images/Пиросманашвили_Рыбак среди скал.jpg'
+    },
+    {
+        name:'Бабушкин сад',
+        author:'Поленов',
+        img:'./images/Поленов_Бабушкин сад.jpg'
+    },
+    {
+        name:'Заросший пруд',
+        author:'Поленов',
+        img:'./images/Поленов_Заросший пруд.jpg'
+    },
+    {
+        name:'Заморские гости',
+        author:'Рерих',
+        img:'./images/Рерих_Заморские гости.jpg'
+    },
+    {
+        name:'Метроном',
+        author:'Розанова',
+        img:'./images/Розанова_Метроном.jpg'
+    },
+    {
+        name:'Грачи прилетели',
+        author:'Саврасов',
+        img:'./images/Саврасов_Грачи прилетели.jpg'
+    },
+    {
+        name:'Похищение Европы',
+        author:'Серов',
+        img:'./images/Серов_Похищение Европы.jpg'
+    },
+    {
+        name:'Утро в сосновом лесу',
+        author:'Шишкин',
+        img:'./images/Шишкин_Утро в сосновом лесу.jpg'
+    },
+    {
+        name:'Всадница',
+        author:'Брюллов',
+        img:'./images/Брюллов_Всадница.jpg'
+    },
+    {
+        name:'Всадник. Георгий победоносец. Эскиз',
+        author:'Кандинский',
+        img:'./images/Кандинский_Всадник. Георгий победоносец. Эскиз.jpg'
+    },
+    {
+        name:'В лодке',
+        author:'Коровин',
+        img:'./images/Коровин_В лодке.jpg'
+    },
+    {
+        name:'Неизвестная',
+        author:'Крамской',
+        img:'./images/Крамской_Неизвестная.jpg'
+    },
+    {
+        name:'Березовая роща',
+        author:'Куинджи',
+        img:'./images/Куинджи_Берёзовая роща.jpg'
+    },
+    {
+        name:'Масленица',
+        author:'Кустодиев',
+        img:'./images/Кустодиев_Масленица.jpg'
+    },
+    {
+        name:'Золотая осень_Левитан',
+        author:'Левитан',
+        img:'./images/Левитан_Золотая осень.jpg'
+    },
+    {
+        name:'Портрет неизвестной с розой',
+        author:'Левицкий',
+        img:'./images/Левицкий_Портрет неизвестной с розой.jpg'
+    },
+    {
+        name:'Василий Блаженный',
+        author:'Лентулов',
+        img:'./images/Лентулов_Василий Блаженный.jpg'
+    },
+    {
+        name:'Чёрный квадрат',
+        author:'Малевич',
+        img:'./images/Малевич_Чёрный квадрат.jpg'
+    },
+    {
+        name:'Золотая осень_Остроухов',
+        author:'Остроухов',
+        img:'./images/Остроухов_Золотая осень.jpg'
+    },
+    {
+        name:'Рыбак среди скал',
+        author:'Пиросманашвили',
+        img:'./images/Пиросманашвили_Рыбак среди скал.jpg'
+    },
+    {
+        name:'Бабушкин сад',
+        author:'Поленов',
+        img:'./images/Поленов_Бабушкин сад.jpg'
+    },
+    {
+        name:'Заросший пруд',
+        author:'Поленов',
+        img:'./images/Поленов_Заросший пруд.jpg'
+    },
+    {
+        name:'Заморские гости',
+        author:'Рерих',
+        img:'./images/Рерих_Заморские гости.jpg'
+    },
+    {
+        name:'Метроном',
+        author:'Розанова',
+        img:'./images/Розанова_Метроном.jpg'
+    },
+    {
+        name:'Грачи прилетели',
+        author:'Саврасов',
+        img:'./images/Саврасов_Грачи прилетели.jpg'
+    },
+    {
+        name:'Похищение Европы',
+        author:'Серов',
+        img:'./images/Серов_Похищение Европы.jpg'
+    },
+    {
+        name:'Утро в сосновом лесу',
+        author:'Шишкин',
+        img:'./images/Шишкин_Утро в сосновом лесу.jpg'
+    },
+    {
+        name:'Купание красного коня',
+        author:'Петров-Водкин',
+        img:'./images/Петров-Водкин_Купание красного коня.jpg'
+    },
+    {
+        name:'Купание красного коня',
+        author:'Петров-Водкин',
+        img:'./images/Петров-Водкин_Купание красного коня.jpg'
+    }
+]
+
+cardArray.sort(()=>0.5-Math.random())
+
+
+const grid = document.querySelector('#grid')
+const result = document.querySelector('#result')
+const attempts = document.querySelector('#attempts')
+let cardsChosen = []
+let cardsIds = []
+let Score = 0
+let attemptsNum = 0
+
+result.innerHTML = Score;
+attempts.innerHTML = attemptsNum;
+
+function createBoard(){
+    for(let i=0; i<cardArray.length;i++){
+        const card = document.createElement('img')
+        card.setAttribute('src','./images/Рубашка.jpg')
+        card.setAttribute('data-id', i)
+        card.setAttribute('width', '150px'),
+        card.setAttribute('height','150px'),
+        card.addEventListener('click', flipcard),
+        grid.appendChild(card)
+    }
+}
+
+createBoard()
+
+function check(){
+    cards = document.querySelectorAll('img')
+    if(cardsChosen[0]==cardsChosen[1]){
+        cards[cardsIds[0]].setAttribute('src','')
+        cards[cardsIds[1]].setAttribute('src','')
+        cards[cardsIds[0]].removeEventListener('click', flipcard)
+        cards[cardsIds[1]].removeEventListener('click', flipcard)
+        Score++
+    }
+    else{
+        cards[cardsIds[0]].setAttribute('src','./images/Рубашка.jpg')
+        cards[cardsIds[1]].setAttribute('src','./images/Рубашка.jpg')
+    }
+    if(Score == cardArray.length/2){
+        setTimeout(alert('Ура! Победа!'), 500)
+    }
+    cardsChosen=[]
+    cardsIds=[]
+    attemptsNum++;
+    result.innerHTML = Score;
+    attempts.innerHTML = attemptsNum;
+}
+
+function flipcard(){
+    const cardId = this.getAttribute('data-id')
+    cardsIds.push(cardId)
+    cardsChosen.push(cardArray[cardId].name)
+    this.setAttribute('src', cardArray[cardId].img)
+    if(cardsChosen.length==2){
+        setTimeout(check, 500)
+    }
+}
+
+console.log(cardsChosen)
