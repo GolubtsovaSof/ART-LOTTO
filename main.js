6d48ada7d7ceae3bc97c93515ffcad5a2067fa79
GolubtsovaSof <92886622+GolubtsovaSof@users.noreply.github.com>
2022-07-29 21:08:54 +0300

    preloader

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+window.addEventListener('load', () => { /* Страница загружена, включая все ресурсы */
+  const preloader = document.querySelector('.preloader') /* находим блок Preloader */
+  preloader.classList.add('preloader_hidden') /* добавляем ему класс для скрытия */
+})
 const cardArray = [
     {
         name:'Всадница',
